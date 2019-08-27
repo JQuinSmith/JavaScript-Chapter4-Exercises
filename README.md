80671cae496d9485b2f8ab7b3bd8a01d221920e2
QuinWithOneN <jquinsmyth@gmail.com>
2019-08-27 10:41:01 -0500

    Exercises complete up to Cookout

---
+++ README.md
@@ -0,0 +1,1 @@
+# JavaScript-Chapter4-Exercises
---
+++ js/cakes.js
@@ -0,0 +1,23 @@
+const valentinesCakes= {
+    name: "Valentines",
+    icing: "strawberry",
+    cake: "vanilla",
+    month: "february"
+}
+const stPatrickCakes = {
+    name: "St. Patricks",
+    icing: "vanilla",
+    cake: "vanilla",
+    month: "march"
+}
+const backToSchoolCakes = {
+    name: "1st Day of School",
+    icing: "chocolate",
+    cake: "chocolate",
+    month: "August"
+}
+
+
+const debbieArray = [valentinesCakes, stPatrickCakes, backToSchoolCakes];
+
+console.log(debbieArray)
